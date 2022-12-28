06dea5feab0fd2639ca1aed46d7474bba56d6931
Akshay  Kumar <112924666+Akshaykumar2004@users.noreply.github.com>
2022-12-28 09:41:56 +0000

    problem01 in set04

---
+++ set04/problem01.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1,int den1, int num2,int den2,int *res_num, int *res_den);
+void output(int num1,int den1,int num2,int den2,int res_num,int res_den);
+
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the 1st fractional number:\n");
+  scanf("%i%i",num1,den1);
+  printf("Enter the 2nd fractional number:\n");
+  scanf("%i%i",num2,den2);
+}
+
+void add(int num1,int den1,int num2,int den2,int *res_num,int *res_den)
+{
+  if(den1 == den2){
+    *res_num=num1+num2;
+    *res_den=den1;
+  }
+  if(den1 != den2){
+    *res_num=(num1*den2) + (num2*den1);
+    *res_den= den1 * den2;
+  }
+}
+
+void output(int num1,int den1,int num2,int den2,int res_num,int res_den)
+{
+  if(res_num == res_den){
+    printf("The sum fractions is 1\n");
+  }
+  if(res_num != res_den){
+    printf("%d/%d + %d/%d = %d/%d\n",num1,den1,num2,den2,res_num,res_den);
+  }
+}
+
+void main(){
+  int num1,num2,resn,den1,den2,resd;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&resn,&resd);
+  output(num1,den1,num2,den2,resn,resd);
+}
